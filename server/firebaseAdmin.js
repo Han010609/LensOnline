50f6645c9f6c55b834235d85759cb20272980e6b
PK <hanhan010609@gmail.comgit statusgit statusgit config --global user.email hanhan010609@gmail.com>
2025-03-24 13:18:03 +0800

    v6改金鑰

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -7,26 +7,27 @@ import dotenv from "dotenv";
 // 取得當前檔案目錄
 const __dirname = dirname(fileURLToPath(import.meta.url));
 // 讀取 Firebase 服務帳戶金鑰
-const serviceAccount = JSON.parse(
-  readFileSync(__dirname + "/firebase-service-account.json", "utf-8")
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = JSON.parse(
+//   readFileSync(__dirname + "/firebase-service-account.json", "utf-8")
+// );
 
 // 初始化 Firebase Admin
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        projectId: serviceAccount.project_id, // ✅ 確保這裡的 project_id 正確
-      });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    projectId: serviceAccount.project_id, // ✅ 確保這裡的 project_id 正確
+  });
 }
 
 // **驗證 Firebase Token**
 export const verifyFirebaseToken = async (token) => {
-    try {
-      return await admin.auth().verifyIdToken(token);
-    } catch (error) {
-      console.error("❌ Firebase Token 驗證失敗:", error);
-      return null;
-    }
-  };
+  try {
+    return await admin.auth().verifyIdToken(token);
+  } catch (error) {
+    console.error("❌ Firebase Token 驗證失敗:", error);
+    return null;
+  }
+};
 
 export default admin;
